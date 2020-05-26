fd953b07917e841769cf774bd96b32c2b6bd0622
Dillon <ee.tinkerer@gmail.com>
2020-05-26 10:36:56 -0400

    requirements to install pygame (GUI framework)

---
+++ boot/pygame-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Need cmake for FreeType
+# & other dependencies
+# https://stackoverflow.com/a/15368766sudo
+apt install cmake libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
+
+# Install FreeType Raspberry Pi
+# https://git.zsinfo.nl/snippets/8
+wget -O - http://git.zsinfo.nl/snippets/8/raw | bash
+
+# install pygame under sudo because it needs sudo to access framebuffer
+# https://www.pygame.org/wiki/GettingStarted
+sudo python3 -m pip install -U pygame
