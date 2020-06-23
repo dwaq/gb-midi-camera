8efa39226500787fc93c4964dc20332a165b7fb5
Dillon <ee.tinkerer@gmail.com>
2020-06-22 21:17:48 -0400

    draw a basic 8x8 red rectangle grid

--- display.py
+++ display.py
@@ -37,13 +37,26 @@ def displayText(text, size, color, clearScreen):
     # only way to draw text is to overlay it using blit
     screen.blit(text, text.get_rect())
 
-displayText('Giant Board', 30, (200,200,1), True)
+#displayText('Giant Board', 30, (200,200,1), True)
 
 # fill screen with any color
 #screen.fill((100, 100, 100))
 
 # draw on screen with color, location/size
-pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(25, 25, 25, 25))
+#pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(25, 25, 25, 25))
+
+# 8x8 grid of temperature values
+# make a square using the full height (so 320x320 px)
+# grid size = 320 pixel / 8 = 40
+gs = 40
+
+# starting position from left
+ls = (width - height) / 2
+
+for row in range(8):
+    for column in range(8):
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(ls+(gs*row), gs*column, gs-3, gs-3))
+
 
 # refresh the display
 pygame.display.flip()
